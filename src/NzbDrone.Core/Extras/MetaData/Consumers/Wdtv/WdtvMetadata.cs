329e6d601d82d8ad8c6363cb233b43429e20e46b
Mark McDowall <markus.mcd5@gmail.com>
2016-02-06 01:20:22 -0800

    Refactor and clean up

--- src/NzbDrone.Core/Extras/MetaData/Consumers/Wdtv/WdtvMetadata.cs
+++ src/NzbDrone.Core/Extras/MetaData/Consumers/Wdtv/WdtvMetadata.cs
@@ -9,7 +9,7 @@ using System.Xml.Linq;
 using NLog;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.Extensions;
-using NzbDrone.Core.Extras.ExtraFiles;
+using NzbDrone.Core.Extras.Files;
 using NzbDrone.Core.Extras.Metadata.Files;
 using NzbDrone.Core.MediaCover;
 using NzbDrone.Core.MediaFiles;
@@ -42,16 +42,16 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Wdtv
             }
         }
 
-        public override string GetFilenameAfterMove(Series series, EpisodeFile episodeFile, ExtraFile metadataFile)
+        public override string GetFilenameAfterMove(Series series, EpisodeFile episodeFile, MetadataFile metadataFile)
         {
             var episodeFilePath = Path.Combine(series.Path, episodeFile.RelativePath);
 
-            if (metadataFile.MetadataType == MetadataType.EpisodeImage)
+            if (metadataFile.Type == MetadataType.EpisodeImage)
             {
                 return GetEpisodeImageFilename(episodeFilePath);
             }
 
-            if (metadataFile.MetadataType == MetadataType.EpisodeMetadata)
+            if (metadataFile.Type == MetadataType.EpisodeMetadata)
             {
                 return GetEpisodeMetadataFilename(episodeFilePath);
             }
@@ -60,17 +60,16 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Wdtv
             return Path.Combine(series.Path, metadataFile.RelativePath);
         }
 
-        public override ExtraFile FindMetadataFile(Series series, string path)
+        public override MetadataFile FindMetadataFile(Series series, string path)
         {
             var filename = Path.GetFileName(path);
 
             if (filename == null) return null;
 
-            var metadata = new ExtraFile
+            var metadata = new MetadataFile
                            {
-                               Type = ExtraType.Metadata,
                                SeriesId = series.Id,
-                               MetadataConsumer = GetType().Name,
+                               Consumer = GetType().Name,
                                RelativePath = series.Path.GetRelativePath(path)
                            };
 
@@ -81,7 +80,7 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Wdtv
                 var seasonMatch = SeasonImagesRegex.Match(parentdir.Name);
                 if (seasonMatch.Success)
                 {
-                    metadata.MetadataType = MetadataType.SeasonImage;
+                    metadata.Type = MetadataType.SeasonImage;
 
                     if (seasonMatch.Groups["specials"].Success)
                     {
@@ -96,7 +95,7 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Wdtv
                     return metadata;
                 }
 
-                metadata.MetadataType = MetadataType.SeriesImage;
+                metadata.Type = MetadataType.SeriesImage;
                 return metadata;
             }
 
@@ -108,10 +107,10 @@ namespace NzbDrone.Core.Extras.Metadata.Consumers.Wdtv
                 switch (Path.GetExtension(filename).ToLowerInvariant())
                 {
                     case ".xml":
-                        metadata.MetadataType = MetadataType.EpisodeMetadata;
+                        metadata.Type = MetadataType.EpisodeMetadata;
                         return metadata;
                     case ".metathumb":
-                        metadata.MetadataType = MetadataType.EpisodeImage;
+                        metadata.Type = MetadataType.EpisodeImage;
                         return metadata;
                 }
                 
